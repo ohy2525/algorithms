b75c3e05e36165c99b0cafefdc0d6af2a55a8d77
ohy2525 <dhgodbs2@naver.com>
2023-07-13 00:11:31 +0900

    10926번

---
+++ java/bj10926.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class bj10926 {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        String lastString = "??!";
+
+        String s = in.next();
+        System.out.println(s + lastString);
+    }
+}
