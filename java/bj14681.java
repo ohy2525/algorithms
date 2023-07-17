022f983c71c95fe4fa0d8d4758b7bb462558f8cf
ohy2525 <dhgodbs2@naver.com>
2023-07-17 16:38:55 +0900

    브론즈 5

---
+++ java/bj14681.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class bj14681 {
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        int x = in.nextInt();
+        int y = in.nextInt();
+        in.close();
+
+        if (x > 0) {
+            if (y > 0) System.out.print(1);
+            else System.out.print(4);
+        }
+        else {
+            if (y > 0) System.out.print(2);
+            else System.out.print(3);
+        }
+    }
+}
