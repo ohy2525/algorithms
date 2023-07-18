95ac878a686676c74585f1d4c9516417befb4f67
ohy2525 <dhgodbs2@naver.com>
2023-07-18 20:44:03 +0900

    브론즈 5

---
+++ java/bj2739.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class bj2739 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        br.close();
+
+        for (int i = 1; i < 10; i++) {
+            System.out.println(N + " * " + i + " = " + N * i);
+        }
+    }
+}
