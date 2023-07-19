0627adacbb6f3fc1bafcaad0fad8badd7fd8d5da
ohy2525 <dhgodbs2@naver.com>
2023-07-19 23:29:43 +0900

    브론즈 5

---
+++ java/bj8393.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class bj8393 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        br.close();
+
+        int ans = 0;
+
+        for (int i = 1; i <= n; i++) {
+            ans += i;
+        }
+        System.out.println(ans);
+    }
+}
