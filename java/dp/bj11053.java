082c840306f18fba1be7cb2e1581bb696abea0a2
ohy <dhgodbs2@naver.com>
2023-09-16 19:25:54 +0900

    실버 2

---
+++ java/dp/bj11053.java
@@ -0,0 +1,38 @@
+package dp;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class bj11053 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+        
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        
+        int[] dp = new int[n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (arr[i] > arr[j] && dp[i] < dp[j]) {
+                    dp[i] = dp[j];
+                }
+            }
+            dp[i]++;
+        }
+        
+        int max = 0;
+        for (int i = 0; i < n; i++) {
+            if (dp[i] > max) {
+                max = dp[i];
+            }
+        }
+        
+        System.out.println(max);
+    }
+}
