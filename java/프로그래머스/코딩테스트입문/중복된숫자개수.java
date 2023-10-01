cb43498b1774050df9abe16ed6a1f0bc724a60c7
ohy2525 <dhgodbs2@naver.com>
2023-10-02 00:10:55 +0900

    프로그래머스 입문

---
+++ java/프로그래머스/코딩테스트입문/중복된숫자개수.java
@@ -0,0 +1,13 @@
+package 프로그래머스.코딩테스트입문;
+
+public class 중복된숫자개수 {
+    public int solution(int[] array, int n) {
+        int answer = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == n) {
+                answer++;
+            }
+        }
+        return answer;
+    }
+}
