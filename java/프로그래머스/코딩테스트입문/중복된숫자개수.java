d061892a8d0a01da4638c0db95f5d006c4944a54
ohy2525 <dhgodbs2@naver.com>
2023-10-02 00:11:21 +0900

    프로그래머스 입문

--- java/프로그래머스/코딩테스트입문/중복된숫자개수.java
+++ java/프로그래머스/코딩테스트입문/중복된숫자개수.java
@@ -1,5 +1,6 @@
 package 프로그래머스.코딩테스트입문;
 
+//https://school.programmers.co.kr/learn/courses/30/lessons/120583?language=java
 public class 중복된숫자개수 {
     public int solution(int[] array, int n) {
         int answer = 0;
