8116753aa3b027f134a4ebe7d73ff0310da654f2
ohy2525 <dhgodbs2@naver.com>
2023-10-04 23:19:08 +0900

    프로그래머스 입문

---
+++ java/프로그래머스/코딩테스트입문/두수의합.java
@@ -0,0 +1,8 @@
+package 프로그래머스.코딩테스트입문;
+
+//https://school.programmers.co.kr/learn/courses/30/lessons/120802
+public class 두수의합 {
+    public int solution(int num1, int num2) {
+        return num1 + num2;
+    }
+}
