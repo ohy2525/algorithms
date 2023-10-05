416bb5022851d0591d06168880ff67297f2251b0
ohy2525 <dhgodbs2@naver.com>
2023-10-05 21:03:44 +0900

    프로그래머스 입문

---
+++ java/프로그래머스/코딩테스트입문/숫자비교하기.java
@@ -0,0 +1,10 @@
+package 프로그래머스.코딩테스트입문;
+
+public class 숫자비교하기 {
+    public int solution(int num1, int num2) {
+        if (num1 == num2) {
+            return 1;
+        }
+        return -1;
+    }
+}
