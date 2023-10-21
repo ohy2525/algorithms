59b0e070065ff985fe18158c05c03ea95f47cc7e
ohy2525 <81752763+ohy2525@users.noreply.github.com>
2023-10-21 22:19:19 +0900

    정렬

---
+++ java/Doit/ch05/소트인사이드.java
@@ -0,0 +1,23 @@
+package Doit.ch05;
+
+import java.io.*;
+import java.util.*;
+
+public class 소트인사이드 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String input = br.readLine();
+        br.close();
+
+        int[] arr = new int[input.length()];
+
+        for (int i = 0; i < input.length(); i++) {
+            arr[i] = Integer.parseInt(input.charAt(i));
+        }
+
+        Arrays.sort(arr);
+        for (int i = arr.length - 1; i >= 0; i--) {
+            System.out.print(arr[i]);
+        }
+    }
+}
---
+++ java/Doit/ch05/좌표정렬하기2.java
@@ -0,0 +1,10 @@
+package Doit.ch05;
+
+import java.io.*;
+import java.util.*;
+
+public class 좌표정렬하기2 {
+    public static void main(String[] args) throws IOException {
+
+    }
+}
