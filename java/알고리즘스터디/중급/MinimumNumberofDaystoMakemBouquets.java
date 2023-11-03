fd2f694516e3026d821b3a2fb795952d63daf090
ohy2525 <dhgodbs2@naver.com>
2023-11-03 16:20:25 +0900

    이분 탐색

---
+++ java/알고리즘스터디/중급/MinimumNumberofDaystoMakemBouquets.java
@@ -0,0 +1,41 @@
+package 알고리즘스터디.중급;
+
+import java.util.*;
+
+public class MinimumNumberofDaystoMakemBouquets {
+
+    public int minDays(int[] bloomDay, int m, int k) {
+        if ((long) m * k > bloomDay.length) {
+            return -1;
+        }
+
+        int left = Arrays.stream(bloomDay).min().getAsInt();
+        int right = Arrays.stream(bloomDay).max().getAsInt();
+
+        while (left < right) {
+            int mid = (left + right) / 2;
+            if (getCount(bloomDay, k, mid) >= m) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        return left;
+    }
+
+    private int getCount(int[] bloomDay, int k, int day) {
+        int bouquetCount = 0;
+        int requiredBouquet = k;
+        for (int n : bloomDay) {
+            if (n > day) {
+                requiredBouquet = k;
+            } else if (--requiredBouquet == 0) {
+                bouquetCount++;
+                requiredBouquet = k;
+            }
+        }
+
+        return bouquetCount;
+    }
+}
