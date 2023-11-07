d638316a29dea6eb4f2838490c83483644b439ca
ohy2525 <dhgodbs2@naver.com>
2023-11-07 23:37:43 +0900

    dp

--- java/알고리즘스터디/중급/LongestIncreasingSubsequence.java
+++ java/알고리즘스터디/중급/LongestIncreasingSubsequence.java
@@ -1,4 +1,18 @@
 package 알고리즘스터디.중급;
 
+import java.util.*;
+
 public class LongestIncreasingSubsequence {
+    public int lengthOfLIS(int[] nums) {
+        int[] dp = new int[nums.length];
+        for (int i = 1; i < nums.length; i++) {
+            for (int j = 0; j < i; j++) {
+                if (nums[i] > nums[j]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+        }
+
+        return Arrays.stream(dp).max().getAsInt() + 1;
+    }
 }
