c77e0a66a543c06b74357b77e394aa402a7177d2
ohy2525 <dhgodbs2@naver.com>
2023-11-19 22:18:13 +0900

    PriorityQueue

--- java/알고리즘스터디/중급/TopKFrequentElements.java
+++ java/알고리즘스터디/중급/TopKFrequentElements.java
@@ -26,34 +26,14 @@ public class TopKFrequentElements {
 
         int[] ans = new int[k];
 
-        PriorityQueue<Integer> q = new PriorityQueue<>(new Comparator<Integer>() {
-            @Override
-            public int compare(Integer o1, Integer o2) {
-                return map.get(o2) - map.get(o1);
-            }
-        });
+        PriorityQueue<Map.Entry<Integer, Integer>> pq = new PriorityQueue<>((a, b) -> a.getValue() - b.getValue());
 
-        q.addAll(map.keySet());
-
-        for (int i = 0; i < k; i++) {
-            ans[i] = q.poll();
+        for (Map.Entry<Integer, Integer> e : map.entrySet()) {
+            pq.add(e);
         }
 
-        return ans;
-    }
-
-    public int[] topKFrequent3(int[] nums, int k) {
-        HashMap<Integer, Integer> map = new HashMap<>();
-        for (int num : nums) {
-            map.put(num, map.getOrDefault(num, 0) + 1);
-        }
-
-        List<Map.Entry<Integer, Integer>> entryList = new ArrayList<>(map.entrySet());
-        entryList.sort((a, b) -> b.getValue().compareTo(a.getValue()));
-
-        int[] ans = new int[k];
         for (int i = 0; i < k; i++) {
-            ans[i] = entryList.get(i).getKey();
+            ans[i] = pq.poll().getKey();
         }
 
         return ans;
