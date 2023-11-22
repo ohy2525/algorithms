1c08c48cc9be8e7e6e0e19d0a786827af4e22cf5
ohy2525 <dhgodbs2@naver.com>
2023-11-22 23:01:14 +0900

    2D dp

--- java/알고리즘스터디/중급/UniquePaths.java
+++ java/알고리즘스터디/중급/UniquePaths.java
@@ -1,5 +1,20 @@
 package 알고리즘스터디.중급;
 
+import java.util.*;
+
 public class UniquePaths {
-    
+    public int uniquePaths(int m, int n) {
+        int[][] dp = new int[m][n];
+        for (int[] arr : dp) {
+            Arrays.fill(arr, 1);
+        }
+
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+            }
+        }
+
+        return dp[m - 1][n - 1];
+    }
 }
