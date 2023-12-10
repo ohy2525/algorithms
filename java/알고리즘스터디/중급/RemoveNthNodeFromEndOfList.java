6f13398de5f40a6087387f612e8668cbe9b0c9ac
ohy2525 <dhgodbs2@naver.com>
2023-12-10 15:06:19 +0900

    LinkedList

---
+++ java/알고리즘스터디/중급/RemoveNthNodeFromEndOfList.java
@@ -0,0 +1,40 @@
+package 알고리즘스터디.중급;
+
+public class RemoveNthNodeFromEndOfList {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode dummy = new ListNode();
+        dummy.next = head;
+        ListNode fast = dummy;
+
+        for (int i = 0; i < n; i++) {
+            fast = fast.next;
+        }
+
+        ListNode slow = dummy;
+        while (fast.next != null) {
+            fast = fast.next;
+            slow = slow.next;
+        }
+
+        slow.next = slow.next.next;
+
+        return dummy.next;
+    }
+
+    class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
