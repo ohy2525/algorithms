6fa2e9634be2611b2a7dd9ebffd586fb1ef50e0e
ohy2525 <81752763+ohy2525@users.noreply.github.com>
2023-12-24 15:59:33 +0900

    구현

--- java/알고리즘스터디/고급/TextJustification.java
+++ java/알고리즘스터디/고급/TextJustification.java
@@ -6,33 +6,52 @@ import java.util.List;
 public class TextJustification {
     public List<String> fullJustify(String[] words, int maxWidth) {
         List<String> ans = new ArrayList<>();
-        int i = 0;
-        int width = 0;
-        List<String> wordList = new ArrayList<>();
-        while (i < words.length) {
-            width += words[i].length();
-            if (width <= maxWidth) {
-                width += 1;
-                wordList.add(words[i]);
-                i += 1;
-                continue;
+        
+        List<String> tmp = new ArrayList<>();
+        int curSize = 0, wordSize = 0;
+        for (int i = 0; i < words.length; i++) {
+            String word = words[i];
+
+            curSize += (curSize == 0 ? 0 : 1) + word.length();
+            if (curSize > maxWidth) {
+                result.add(flush(tmp, maxWidth, wordSize, false));
+                i--;
+                tmp = new ArrayList<>();
+                curSize = 0;
+                wordSize = 0;
+            } else {
+                tmp.add(word);
+                wordSize += word.length();
             }
-            StringBuilder sb = new StringBuilder();
-            int tmp = 0;
-            for (String s : wordList) {
-                tmp += s.length();
+        }
+        result.add(flush(tmp, maxWidth, wordSize, true));
+
+        return result;
+    }
+
+    private String flush(List<String> list, int maxWidth, int wordSize, boolean isLast) {
+        int n = list.size();
+        StringBuilder sb = new StringBuilder(list.get(0));
+
+        if (n > 1) {
+            int blankCnt = isLast ? 1 : (maxWidth - wordSize) / (n - 1);
+            int remainingCnt = isLast ? 0 : (maxWidth - wordSize) % (n - 1);
+
+            for (int i = 1; i < n; i++) {
+                for (int j = 0; j < blankCnt; j++) {
+                    sb.append(" ");
+                }
+                if (remainingCnt > 0) {
+                    remainingCnt--;
+                    sb.append(" ");
+                }
             }
-            int quot = (maxWidth - tmp) / (wordList.size() - 1);
-            int rem = (maxWidth - tmp) / (wordList.size() - 1);
-
-
-            width = 0;
-            wordList = new ArrayList<>();
-
         }
 
+        while (sb.length < maxWidth) {
+            sb.append(" ");
+        }
 
-
-        return ans;
+        return sb.toString();
     }
 }
