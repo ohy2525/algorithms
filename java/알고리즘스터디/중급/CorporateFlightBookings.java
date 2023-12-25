3377244ef7fcf4b68c6b020bb21bd40b79db819d
ohy2525 <dhgodbs2@naver.com>
2023-12-25 23:55:55 +0900

    누적합

--- java/알고리즘스터디/중급/CorporateFlightBookings.java
+++ java/알고리즘스터디/중급/CorporateFlightBookings.java
@@ -11,4 +11,28 @@ public class CorporateFlightBookings {
 
         return answer;
     }
+
+    //누적합
+    public int[] corpFlightBookings(int[][] bookings, int n) {
+        int[] ans = new int[n];
+
+        for (int[] booking : bookings) {
+            int tickets = booking[2];
+            int from = booking[0];
+            int to = booking[1];
+
+            ans[from - 1] += tickets;
+            if (to < n) {
+                ans[to] -= tickets;
+            }
+        }
+
+        int currSum = 0;
+        for (int i = 0; i < n; i++) {
+            currSum += ans[i];
+            ans[i] = currSum;
+        }
+
+        return ans;
+    }
 }
