b74544cadc20d1b738381a1eba8e69b49b14e359
ohy2525 <dhgodbs2@naver.com>
2023-12-27 21:13:54 +0900

    dfs

---
+++ java/백준/dfs/bj10026.java
@@ -0,0 +1,64 @@
+package 백준.dfs;
+
+import java.io.*;
+
+public class bj10026 {
+    static int answer;
+    static int n;
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
+    static boolean[][] visited;
+    static char[][] arr;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+        arr = new char[n][n];
+        for (int i = 0; i < n; i++) {
+            String input = br.readLine();
+            for (int j = 0; j < n; j++) {
+                arr[i][j] = input.charAt(j);
+            }
+        }
+        br.close();
+        visited = new boolean[n][n];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (!visited[i][j]) {
+                    dfs(i, j);
+                    answer++;
+                }
+            }
+        }
+        StringBuilder sb = new StringBuilder(answer + " ");
+        answer = 0;
+        visited = new boolean[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                arr[i][j] = arr[i][j] == 'B' ? 'B' : 'R';
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (!visited[i][j]) {
+                    dfs(i, j);
+                    answer++;
+                }
+            }
+        }
+        sb.append(answer);
+        System.out.print(sb);
+    }
+
+    private static void dfs(int x, int y) {
+        visited[x][y] = true;
+        for (int i = 0; i < 4; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+            if (nx >= 0 && nx < n && ny >= 0 && ny < n && arr[nx][ny] == arr[x][y] && !visited[nx][ny]) {
+                dfs(nx, ny);
+            }
+        }
+    }
+}
