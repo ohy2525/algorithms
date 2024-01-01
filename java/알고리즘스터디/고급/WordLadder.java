1c36b1020b561e23caa4a4f46e93967926c257d7
ohy2525 <dhgodbs2@naver.com>
2024-01-01 21:45:33 +0900

    구현

--- java/알고리즘스터디/고급/WordLadder.java
+++ java/알고리즘스터디/고급/WordLadder.java
@@ -1,7 +1,46 @@
 package 알고리즘스터디.고급;
 
+import java.util.*;
+
 public class WordLadder {
     public int ladderLength(String beginWord, String endWord, List<String> wordList) {
-    }
+        Set<String> visited = new HashSet<>();
+        Set<String> words = new HashSet<>(wordList);
+        visited.add(beginWord);
 
+        Queue<String> q = new LinkedList<>();
+        q.add(beginWord);
+
+        int steps = 1;
+        while (!q.isEmpty()) {
+            int size = q.size();
+
+            for (int i = 0; i < size; i++) {
+                String cur = q.poll();
+                char[] arr = cur.toCharArray();
+
+                for (int j = 0; j < cur.length(); j++) {
+                    char c = arr[j];
+                    for (int k = 0; k < 26; k++) {
+                        arr[j] = (char)(k + 'a');
+                        if (arr[j] != c) {
+                            String target = new String(arr);
+                            if (words.contains(target)) {
+                                if (target.equals(endWord)) {
+                                    return steps + 1;
+                                }
+                                if (visited.add(target)) {
+                                    q.add(target);
+                                }
+                            }
+                        }
+                    }
+                    arr[j] = c;
+                }
+            }
+            steps++;
+        }
+
+        return 0;
+    }
 }
