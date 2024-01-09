36e6a64b48d34f9efd71ef5e1065c9d03638e095
ohy2525 <dhgodbs2@naver.com>
2024-01-09 23:36:50 +0900

    구현

--- java/알고리즘스터디/중급/CommitOffset.java
+++ java/알고리즘스터디/중급/CommitOffset.java
@@ -1,9 +1,35 @@
 package 알고리즘스터디.중급;
 
-import java.util.Arrays;
+import java.util.*;
 
 public class CommitOffset {
-    public static int[] commitOffsets(int[] offsets) {
+
+    static int[] commitOffsets(int[] offsets) {
+        int[] result = new int[offsets.length];
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < offsets.length; i++) {
+            int offset = offsets[i];
+            map.put(offset, offset);
+            int lowest = offset, highest = offset;
+
+            if (map.containsKey(offset - 1)) {
+                lowest = map.get(offset - 1);
+            }
+            if (map.containsKey(offset + 1)) {
+                highest = map.get(offset + 1);
+            }
+
+            map.put(lowest, highest);
+            map.put(highest, lowest);
+
+            result[i] = lowest == 0 ? highest : -1;
+        }
+
+        return result;
+    }
+
+     static int[] commitOffsets2(int[] offsets) {
         int[] result = new int[offsets.length];
         Arrays.fill(result, -1);
         int max = offsets[0];
