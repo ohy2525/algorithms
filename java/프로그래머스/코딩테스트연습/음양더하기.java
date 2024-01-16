110776e58a7a8ad59c99fa21ff0005ae67cb6f9b
ohy2525 <dhgodbs2@naver.com>
2024-01-17 00:20:09 +0900

    구현

---
+++ java/프로그래머스/코딩테스트연습/음양더하기.java
@@ -0,0 +1,16 @@
+package 프로그래머스.코딩테스트연습;
+
+public class 음양더하기 {
+    public int solution(int[] absolutes, boolean[] signs) {
+        int answer = 0;
+        for (int i = 0; i < signs.length; i++) {
+            if (signs[i]) {
+                answer += absolutes[i];
+            } else {
+                answer -= absolutes[i];
+            }
+        }
+
+        return answer;
+    }
+}
