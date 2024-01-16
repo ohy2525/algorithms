2bd1e351ef64a8fc239c849cbd5d3436f9d461e7
ohy2525 <dhgodbs2@naver.com>
2024-01-16 23:39:18 +0900

    구현

--- java/알고리즘스터디/카카오코딩테스트/비밀지도.java
+++ java/알고리즘스터디/카카오코딩테스트/비밀지도.java
@@ -2,7 +2,46 @@ package 알고리즘스터디.카카오코딩테스트;
 
 public class 비밀지도 {
     public String[] solution(int n, int[] arr1, int[] arr2) {
-        String[] answer = {};
-        return answer;
+        String[] result = new String[n];
+        for (int i = 0; i < n; i++) {
+            StringBuilder sb = new StringBuilder();
+            String binary1 = binary(arr1[i], n);
+            String binary2 = binary(arr2[i], n);
+            for (int j = 0; j < n; j++) {
+                if (binary1.charAt(j) == '0' && binary2.charAt(j) == '0') {
+                    sb.append(" ");
+                } else {
+                    sb.append("#");
+                }
+            }
+            result[i] = sb.toString();
+        }
+
+        return result;
+    }
+
+    public String binary(int num, int n) {
+
+        StringBuilder sb = new StringBuilder();
+        if (num == 0) {
+            for (int i = 0; i < n; i++) {
+                sb.append("0");
+            }
+            return sb.toString();
+        }
+
+        while (num > 0) {
+            int remainder = num % 2;
+            sb.insert(0, remainder);
+            num = num / 2;
+        }
+
+        if (sb.length() < n) {
+            int c = n - sb.length();
+            for (int i = 0; i < c; i++) {
+                sb.insert(0, "0");
+            }
+        }
+        return sb.toString();
     }
 }
