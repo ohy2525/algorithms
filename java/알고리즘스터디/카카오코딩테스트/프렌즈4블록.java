eb03a5c4b47c54da472f1a5f1cad82d81712df21
ohy2525 <dhgodbs2@naver.com>
2024-02-05 23:11:08 +0900

    구현

---
+++ java/알고리즘스터디/카카오코딩테스트/프렌즈4블록.java
@@ -0,0 +1,16 @@
+package 알고리즘스터디.카카오코딩테스트;
+
+import java.util.*;
+
+public class 프렌즈4블록 {
+    public int solution(int m, int n, String[] board) {
+        int answer = 0;
+        char[][] map = new char[m][n];
+
+        for (int i = 0; i < m; i++) {
+            map[i] = board[i].toCharArray();
+        }
+
+
+    }
+}
