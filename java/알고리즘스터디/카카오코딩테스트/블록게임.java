2671e6ce54c9e2a5cfc8f5eea99e5530d1513c08
ohy2525 <dhgodbs2@naver.com>
2024-02-09 21:05:47 +0900

    구현

--- java/알고리즘스터디/카카오코딩테스트/블록게임.java
+++ java/알고리즘스터디/카카오코딩테스트/블록게임.java
@@ -1,9 +1,40 @@
 package 알고리즘스터디.카카오코딩테스트;
 
 public class 블록게임 {
-    public int solution(int[][] board) {
-        int answer = 0;
+    int N;
+    int[][] Board;
 
+    public int solution(int[][] board) {
+        N = board.length;
+        Board = board;
+        int answer = 0;
+        int cnt;
+
+        do {
+            cnt = 0;
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < N; j++) {
+                    if (find(i, j, 2, 3)) {
+                        cnt++;
+                    } else if (find(i, j, 3, 2)) {
+                        cnt++;
+                    }
+                }
+            }
+            answer += cnt;
+        } while (cnt != 0):
         return answer;
+    }
+
+    private boolean find(int row, int col, int h, int w) {
+        int emptyCnt = 0;
+        int lastValue = -1;
+        for (int r = row; r < row + h; r++) {
+            for (int c = col; c < col + w; c++) {
+                if (Board[r][c] == 0) {
+                    
+                }
+            }
+        }
     }
 }
