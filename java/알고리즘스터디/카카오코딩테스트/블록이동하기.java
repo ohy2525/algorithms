8fc510c1b69610a031a7bd4ea7da3cca23bad6c0
ohy2525 <dhgodbs2@naver.com>
2024-03-04 22:00:50 +0900

    구현

---
+++ java/알고리즘스터디/카카오코딩테스트/블록이동하기.java
@@ -0,0 +1,72 @@
+package 알고리즘스터디.카카오코딩테스트;
+
+import java.util.*;
+
+public class 블록이동하기 {
+    public int solution(int[][] board) {
+        int n = board.length;
+        Set<String> visited = new HashSet<>();
+        visited.add("0_0_0");
+        Queue<int[]> q = new LinkedList<>();
+        q.add(new int[]{0, 0, 0});
+        int steps = 0;
+
+        while (!q.isEmpty()) {
+            int size = q.size();
+            for (int i = 0; i < size; i++) {
+                int[] cur = q.poll();
+                int y = cur[0], x = cur[1], o = cur[2];
+
+                if ((y == n - 1 && x == n - 2 && o == 0)
+                        || (y == n - 2 && x == n - 1 && o == 1)) {
+                    return steps;
+                }
+
+                if (o == 0) {
+                    if (x < n - 2 && board[y][x + 2] == 0) {
+                        handle(y, x + 1, 0, q, visited);
+                    }
+                    if (x > 0 && board[y][x - 1] == 0) {
+                        handle(y, x - 1, 0, q, visited);
+                    }
+                    if (y < n - 1 && board[y + 1][x] == 0 && board[y + 1][x + 1] == 0) {
+                        handle(y + 1, x, 0, q, visited);
+                        handle(y, x, 1, q, visited);
+                        handle(y, x + 1, 1, q, visited);
+                    }
+                    if (y > 0 && board[y - 1][x] == 0 && board[y - 1][x + 1] == 0) {
+                        handle(y - 1, x, 0, q, visited);
+                        handle(y - 1, x, 1, q, visited);
+                        handle(y - 1, x + 1, 1, q, visited);
+                    }
+                } else {
+                    if (x < n - 1 && board[y][x + 1] == 0 && board[y + 1][x + 1] == 0) {
+                        handle(y, x + 1, 1, q, visited);
+                        handle(y, x, 0, q, visited);
+                        handle(y + 1, x, 0, q, visited);
+                    }
+                    if (x > 0 && board[y][x - 1] == 0 && board[y + 1][x - 1] == 0) {
+                        handle(y, x - 1, 1, q, visited);
+                        handle(y, x - 1, 0, q, visited);
+                        handle(y + 1, x - 1, 0, q, visited);
+                    }
+                    if (y < n - 2 && board[y + 2][x] == 0) {
+                        handle(y + 1, x, 1, q, visited);
+                    }
+                    if (y > 0 && board[y - 1][x] == 0) {
+                        handle(y - 1, x, 1, q, visited);
+                    }
+                }
+            }
+            steps++;
+        }
+
+        return -1;
+    }
+
+    private void handle(int y, int x, int orientation, Queue<int[]> q, Set<String> visited) {
+        if (visited.add(String.format("%d_%d_%d", y, x, orientation))) {
+            q.add(new int[]{y, x, orientation});
+        }
+    }
+}
