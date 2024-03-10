55543c2aec854f3b005290724ff65e214e841004
ohy2525 <dhgodbs2@naver.com>
2024-03-10 23:10:20 +0900

    구현

--- java/알고리즘스터디/카카오코딩테스트/문자열압축.java
+++ java/알고리즘스터디/카카오코딩테스트/문자열압축.java
@@ -26,4 +26,36 @@ public class 문자열압축 {
 
         return answer;
     }
+
+    public int solution2(String s) {
+        int answer = s.length();
+
+        for (int cnt = 1; cnt <= s.length() / 2; cnt++) {
+            StringBuilder sb = new StringBuilder();
+
+            String cur = "";
+            int count = 1, i = 0;
+            while (i < s.length()) {
+                StringBuilder tmp = new StringBuilder();
+                for (int j = 0; j < cnt; j++) {
+                    if (i == s.length()) break;
+                    tmp.append(s.charAt(i++));
+                }
+
+                String target = tmp.toString();
+                if (cur.equals(target)) {
+                    count++;
+                } else {
+                    sb.append(count == 1 ? "" : String.valueOf(cnt)).append(cur);
+                    cur = target;
+                    count = 1;
+                }
+            }
+            sb.append(count == 1 ? "" : String.valueOf(count)).append(cur);
+
+            answer = Math.min(answer, sb.length());
+        }
+
+        return answer;
+    }
 }
