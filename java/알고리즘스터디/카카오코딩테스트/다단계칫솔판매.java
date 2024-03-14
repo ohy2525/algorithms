6a8e4a037d03904ae84c4c9ed7a7c70744360931
ohy2525 <dhgodbs2@naver.com>
2024-03-14 23:05:44 +0900

    트리

---
+++ java/알고리즘스터디/카카오코딩테스트/다단계칫솔판매.java
@@ -0,0 +1,8 @@
+package 알고리즘스터디.카카오코딩테스트;
+
+class 다단계칫솔판매 {
+    public int[] solution(String[] enroll, String[] referral, String[] seller, int[] amount) {
+        int[] answer = {};
+        return answer;
+    }
+}
