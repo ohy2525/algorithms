ab11e02da0b8796cd90f9196901fdc4a1a76d5a2
ohy2525 <dhgodbs2@naver.com>
2024-04-17 23:41:47 +0900

    구현

---
+++ java/알고리즘스터디/카카오코딩테스트/개인정보수집유효기간.java
@@ -0,0 +1,32 @@
+package 알고리즘스터디.카카오코딩테스트;
+
+import java.util.*;
+
+public class 개인정보수집유효기간 {
+    public static List<Integer> solution(String today, String[] terms, String[] privacies) {
+        List<Integer> answer = new ArrayList<>();
+        String[] todaySplit = today.split("\\.");
+        int today_ = Integer.parseInt(todaySplit[0]) * 12 * 28 + Integer.parseInt(todaySplit[1]) * 28 + Integer.parseInt(todaySplit[2]);
+
+        Map<String, Integer> termsMap = new HashMap<>();
+        for (String term : terms) {
+            String[] t = term.split(" ");
+            termsMap.put(t[0], Integer.parseInt(t[1]));
+        }
+        int idx = 1;
+        for (String privacy : privacies) {
+            String[] tmp = privacy.split(" ");
+            String p = tmp[1];
+            Integer term = termsMap.get(p);
+            String[] date = tmp[0].split("\\.");
+            int target = term * 28 + Integer.parseInt(date[0]) * 12 * 28 + Integer.parseInt(date[1]) * 28 + Integer.parseInt(date[2]);
+            System.out.println(today_  + "#####" + target);
+            if (today_ >= target) {
+                answer.add(idx);
+            }
+            idx++;
+        }
+
+        return answer;
+    }
+}
