9c3582ea0fabfab3a3c4a3b1427fb812ad5d4216
ohy2525 <dhgodbs2@naver.com>
2024-10-03 23:24:15 +0900

    구현

---
+++ java/프로그래머스/로또의최고순위와최저순위.java
@@ -0,0 +1,26 @@
+package 프로그래머스;
+
+public class 로또의최고순위와최저순위 {
+    public int[] solution(int[] lottos, int[] win_nums) {
+        int[] answer = new int[2];
+        int cnt = 0;
+        int zeroCnt = 0;
+        for (int lotto : lottos) {
+            if (lotto == 0) {
+                zeroCnt++;
+            } else {
+                for (int num : win_nums) {
+                    if (lotto == num) {
+                        cnt++;
+                        break;
+                    }
+                }
+            }
+        }
+
+        answer[0] = cnt + zeroCnt == 0 ? 6 : 7 - (cnt + zeroCnt);
+        answer[1] = cnt == 0 ? 6 : 7 - cnt;
+
+        return answer;
+    }
+}
